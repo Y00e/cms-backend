95567e8048bfbd55c6bba3ffe41ce77ad73b415a
YoussefMu23 <148534137+YoussefMu23@users.noreply.github.com>
2024-02-18 13:41:41 +0100

    Update ChannelController.java
    added ResponseEntity and status code to the REST API s

--- src/main/java/com/example/cms/controller/ChannelController.java
+++ src/main/java/com/example/cms/controller/ChannelController.java
@@ -4,6 +4,7 @@ import com.example.cms.model.Channel;
 import com.example.cms.model.Message;
 import com.example.cms.service.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,61 +21,98 @@ public class ChannelController {
 
     // GET http://localhost:8080/api/v1/channels
     @GetMapping
-    public List<Channel> getAllChannels(){
-        return channelService.getAll();
+    public ResponseEntity<List<Channel>> getAllChannels(){
+       List<Channel> channels = channelService.getAll();
+
+        return ResponseEntity.status(200).body(channels);
     }
 
     // GET http://localhost:8080/api/v1/channels/search/{title}
     @GetMapping("search/{title}")
-    public Channel getChannelByTitle (@PathVariable String title) {
-        return channelService.getChannelByTitle(title);
+    public ResponseEntity<Channel> getChannelByTitle (@PathVariable String title) {
+         Channel channel = channelService.getChannelByTitle(title);
+
+         if (channel != null) {
+             return ResponseEntity.status(200).body(channel);
+         }
+        return ResponseEntity.status(404).build();
     }
 
     // POST http://localhost:8080/api/v1/channels
     @PostMapping
-    public String createNewChannel(@RequestBody Channel channel) {
+    public ResponseEntity<String> createNewChannel(@RequestBody Channel channel) {
         channelService.create(channel);
 
-        return "Success";
+        return ResponseEntity.status(201).body("Success");
     }
 
     // DELETE http://localhost:8080/api/v1/channels/{id}
     @DeleteMapping("/{id}")
-    public void deleteChannel(@PathVariable Long id) {
+    public ResponseEntity<String> deleteChannel(@PathVariable Long id) {
         channelService.delete(id);
+
+        return ResponseEntity.status(204).build();
     }
 
     // PATCH http://localhost:8080/api/v1/channels/{id}
     @PatchMapping("/{id}")
-    public Channel updateChannelTitle(@PathVariable Long id, @RequestBody Channel newTitle) {
-        return channelService.updateTitle(id, newTitle.getTitle());
+    public ResponseEntity<Channel> updateChannelTitle(@PathVariable Long id, @RequestBody Channel newTitle) {
+         Channel updatedChannel = channelService.updateTitle(id, newTitle.getTitle());
 
+         if (updatedChannel !=null) {
+             return ResponseEntity.status(200).body(updatedChannel);
+         } else {
+             return ResponseEntity.status(404).build();
+         }
     }
 
 
     // PUT http://localhost:8080/api/v1/channels/{id}
     @PutMapping("/{id}")
-    public Message createMessage(@PathVariable Long id, @RequestBody Message message) {
-        return channelService.createMessage(id, message);
+    public ResponseEntity<Message> createMessage(@PathVariable Long id, @RequestBody Message message) {
+        Message createdMessage = channelService.createMessage(id, message);
+
+        if (createdMessage != null) {
+            return ResponseEntity.status(201).body(createdMessage);
+        } else {
+            return ResponseEntity.status(400).build();
+        }
     }
+
 
     // GET http://localhost:8080/api/v1/channels/{id}
     @GetMapping("/{id}")
-    public List<Message> getChannelAllMessages(@PathVariable Long id) {
-        return channelService.getMessages(id);
+    public ResponseEntity <List<Message>> getChannelAllMessages(@PathVariable Long id) {
+        List<Message> messages = channelService.getMessages(id);
+
+        if (messages != null && !messages.isEmpty()) {
+            return ResponseEntity.status(200).body(messages);
+        } else {
+            return ResponseEntity.status(204).build();
+        }
+
     }
 
     // PUT http://localhost:8080/api/v1/channels/message/{id}
     @PutMapping("message/{id}")
-    public Message updateMessage(@PathVariable Long id, @RequestBody Message newText) {
-       return channelService.updateMessage(id, newText.getText());
+    public ResponseEntity<Message>  updateMessage(@PathVariable Long id, @RequestBody Message newText) {
+       Message updatedMessage = channelService.updateMessage(id, newText.getText());
+
+       if (updatedMessage !=null) {
+           return ResponseEntity.status(200).body(updatedMessage);
+       } else {
+           return ResponseEntity.status(400).build();
+       }
+
     }
+
 
     // DELETE http://localhost:8080/api/v1/channels/message/{id}
     @DeleteMapping("message/{id}")
-    public void deleteMessage(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteMessage(@PathVariable Long id) {
         channelService.delete(id);
+
+        return ResponseEntity.status(204).build();
     }
-
-
+    
 }
